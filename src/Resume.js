34973cb53fddb92bae90dddcb811b5bd8291d4f1
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-24 00:44:18 -0400

    Switch to sass, refine layout a bit, lorem ipsum placeholder text

--- src/Resume.js
+++ src/Resume.js
@@ -1,40 +1,46 @@
 import React from 'react';
-import './Resume.css';
+import './Resume.scss';
+
+const lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 function Resume(){
   return (
-    <div style={{display: "flex"}} className="resume">
-      <section className="fcol fcol__1-3rd-page">
+    <div className="resume fcol">
+      <section className="frow">
         <h1>
           {"Stephen O'Neil"}
         </h1>
-        <h2>
-          {"Developer"}
-        </h2>
+        {lorem_ipsum}
       </section>
-      <div className="fcol">
-        <section className="fcol fcol__2-3rd-page">
+      <div className="frow" style={{flexGrow: 1}}>
+        <section className="fcol fcol__1-3rd">
           <h2>
-            {"Contact"}
+            {"Developer"}
           </h2>
+          {lorem_ipsum}
         </section>
-        <div className="frow">
-          <section className="fcol fcol__1-3rd-page">
+        <div className="fcol fcol__2-3rd">
+          <div className="frow">
+            <section className="fcol fcol__1-half">
+              <h2>
+                {"Operations"}
+              </h2>
+              {lorem_ipsum}
+            </section>
+            <section className="fcol fcol__1-half">
+              <h2>
+                {"Security"}
+              </h2>
+              {lorem_ipsum}
+            </section>
+          </div>
+          <section className="fcol" style={{height: "100%"}}>
             <h2>
-              {"Operations"}
+              {"CV"}
             </h2>
-          </section>
-          <section className="fcol fcol__1-3rd-page">
-            <h2>
-              {"Security"}
-            </h2>
+            {lorem_ipsum}
           </section>
         </div>
-        <section className="fcol fcol__2-3rd-page">
-          <h2>
-            {"CV"}
-          </h2>
-        </section>
       </div>
     </div>
   );
