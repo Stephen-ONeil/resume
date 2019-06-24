428060e113573cd01fdc2cec8f429c0f6a7df814
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-23 20:22:02 -0400

    Set up project to be a resume page

---
+++ src/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Resume.css';
+
+function Resume() {
+  return (
+    <div style={{display: "flex"}} className="resume">
+      <p> bleh </p>
+      <p style={{alignSelf: "flex-end"}}> blah </p>
+    </div>
+  );
+}
+
+export { Resume };
