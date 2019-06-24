036d49054ce90627b2dc19dae140bbbc3916e4a6
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-23 20:53:17 -0400

    Slap together resume layout

--- src/Resume.js
+++ src/Resume.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './Resume.css';
 
-function Resume() {
+function Resume(){
   return (
     <div style={{display: "flex"}} className="resume">
-      <p> bleh </p>
-      <p style={{alignSelf: "flex-end"}}> blah </p>
+      <section className="fcol fcol__1-3rd-page">
+        <h1>
+          {"Stephen O'Neil"}
+        </h1>
+        <h2>
+          {"Developer"}
+        </h2>
+      </section>
+      <div className="fcol">
+        <section className="fcol fcol__2-3rd-page">
+          <h2>
+            {"Contact"}
+          </h2>
+        </section>
+        <div className="frow">
+          <section className="fcol fcol__1-3rd-page">
+            <h2>
+              {"Operations"}
+            </h2>
+          </section>
+          <section className="fcol fcol__1-3rd-page">
+            <h2>
+              {"Security"}
+            </h2>
+          </section>
+        </div>
+        <section className="fcol fcol__2-3rd-page">
+          <h2>
+            {"CV"}
+          </h2>
+        </section>
+      </div>
     </div>
   );
 }
