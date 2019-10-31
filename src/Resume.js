18e0ee6e506636bb929ec8f8e6b79dd8ca01159c
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-31 19:02:39 -0400

    Refresh resume content

--- src/Resume.js
+++ src/Resume.js
@@ -9,6 +9,7 @@ const contact_info = `
 ## Full Stack Web App Dev
 (613) 867-7727  
 [github.com/Stephen-ONeil](https://github.com/Stephen-ONeil)  
+[github.com/TBS-EACPD/infobase](https://github.com/TBS-EACPD/infobase)  
 [Stephen.Levi.ONeil@gmail.com](mailto:Stephen.Levi.ONeil@gmail.com)  
 Apt. 7 - 45 Somerset Street, Ottawa, Ontario, K2P 0H3  
 `;
@@ -19,24 +20,24 @@ const brief = `
 Treasury Board of Canada Secretariat (TBS)
   - **4+ years front end**. **React, Lodash, Sass, D3**
   - **1.5+ years back end, DevOps. Node, GraphQL, MongoDB, Google Cloud**
-  - E.g. **GC InfoBase** ([canada.ca/gcinfobase](https://canada.ca/gcinfobase)), a React SPA backed by a serverless GraphQL API
+- E.g. **GC InfoBase** ([canada.ca/gcinfobase](https://canada.ca/gcinfobase)),  a SPA React frontend backed by a serverless GraphQL API, data vizualization and data exploration
 `;
 
 const developer = `
 ## Developer
-- _Prefer done right, right now over "done yesterday". Done yesterday is the enemy of still works tomorrow._  
-&nbsp;&nbsp;&nbsp;&nbsp;– Stephen O'Neil
+- _Prefer done right, now, over done yesterday. The "done yesterday" mindset is opposed to "still works tomorrow"._  
+&nbsp;&nbsp;&nbsp;&nbsp&nbsp&nbsp;– Me, on my resume
 - JS expert, HTML & CSS proficient. Framework of choice React. Strives to make ethical and user-centric design decisions, from speed to A11y
 - ECMAScript proposal watcher, excited for the pipeline operator and optional chaining + nullish coalescing. Ride the wave of JS progress!
-- Generally borrows heavily from the functional paradigm, favours declarative style for internal APIs
-- Most at home with Linux or Posix (MacOS) systems. Comfortable with Windows. Environment of choice is VS Code and command line tools
-- Git and GitHub fan, with years of experience using and helping others to make full use of the tool & service
+- Generally borrows heavily from the functional paradigm, favours declarative APIs internally, and loves formulating robust data-driven control-flow  
+- Environment of choice is VS Code and command line tools. Most at home with Linux or Posix (macOS) systems.
+- Git and GitHub fan, with years of experience using and helping others to learn the tool & service
 `;
 
 const operations = `
 ## Operations                
-- Strong believer in DevOps, personally bringing both the "Dev" and the "Ops" to all projects where feasible
-- Automation and Bash scripts! Serverless microservices! Logging! Actually using logs! Making deployments easy & safe!
+- Strong believer in DevOps, personally bringing both the "Dev" and the "Ops" to the greatest feasible extent
+- Automation and Bash scripts! Getting fancy with CI! Serverless microservices! Easy & safe deploys! It's rewarding and fun.
 `;
 
 const security = `
@@ -49,15 +50,15 @@ const curriculum_vitae = `
 ## Curriculum Vitae
 - **09/2017 - Ongoing** – Senior JavaScript Developer, Expenditure Management Sector (EMS), TBS
   - Maintaining the GC InfoBase front end source code, at ~50,000 lines of JS. Implementing, and guiding junior Devs' implementation of new features while ensuring continued maintainability
-  - Building backend components for the main SPA, including a GraphQL data-fetching API and an email backend (obviating mailto)
+  - Building backend components for the main SPA, including a GraphQL data-fetching API and an email backend (obviating mailto:)
   - Hiring, supervising, and mentoring of CO-OP students; 7+ to date
-  - Championing for code standards, code review, testing, CI/CD, and Open Sourcing/working in the open
+  - Championing for code standards, code review, testing, CI/CD, and Open Sourcing/working in the open. Recent success: finally OS!
   - Transitioned hosting from a static file server administered by a separate team to a self-administered cloud hosting setup, bringing the deployment process from days to minutes, gaining more flexible, faster hosting & a real backend, and doing it all for negligible hosting costs
   - Providing a security audit function within my team, identifying more relevant risks and bugs than IT commissioned App scans
   
-- **01/2015 - 09/2017** – Developer/Junior Analyst, Office of the Chief Human Resources Officer (OCHRO), TBS
-  - Developed and maintained OCHRO's portions of the GC InfoBase web app, in partnership with EMS colleagues
-  - SQL, SAS, Java. Responded to ad-hoc data requests and automated periodic ones
+- **01/2015 - 09/2017** – Developer/Junior Analyst/CO-OP Student, Office of the Chief Human Resources Officer (OCHRO), TBS
+  - Developed and maintained OCHRO's portions of the GC InfoBase
+  - SQL, SAS, Java. Responded to ad-hoc requests for government-wide HR data, automated the periodic ones
   
 - **09/2012 - 04/2017** – Honours Bachelour of Science, Physics-Mathematics with CO-OP, University of Ottawa
   - Dean's Honour List 2014-2015 (for an annual GPA of 8.5+)
@@ -66,8 +67,8 @@ const curriculum_vitae = `
 
 const extracurricular = `
 ## Extracurricular
-- Regular attendee at the Ottawa Civic Tech meet-up
-- GNU+Linux/unix nerd
+- *nix nerd, [github.com/ Stephen-ONeil/.dotfiles](https://github.com/Stephen-ONeil/.dotfiles)
+- Local tech meet-ups
 - Reading novels (Sci-fi, fantasy) & comics (web, indie)
 - Listening to music (misc) & podcasts (comedy, tech)
 - Night-time walking
