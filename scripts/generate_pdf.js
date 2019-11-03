521a012457de194c55e2e58791dbdc05b501d7df
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-02 23:53:42 -0400

    Improve generate_pdf.js script, have it run its own serve out of the build dir instead of assuming its served on port 3000 already

--- scripts/generate_pdf.js
+++ scripts/generate_pdf.js
@@ -1,11 +1,44 @@
-const puppeteer = require('puppeteer');
 const path = require('path');
+const fs = require('fs');
+const http = require('http');
 
-// Assumes resume is being served locally on port 3000
+const getPort = require('get-port');
+const handler = require('serve-handler');
+const puppeteer = require('puppeteer');
+
+
+const pdf_name = 'stephen-oneil-resume.pdf';
+
+const build_dir = path.resolve(__dirname, '../build');
+
+const serve_build_dir = async (port) =>  http
+  .createServer( (request, response) => handler(request, response, {public: build_dir}) )
+  .listen(port);
+
+const save_pdf = async (port) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${port}/`); 
+  await page.pdf({path: `${build_dir}/${pdf_name}`});
+  await browser.close();
+};
+
+
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/'); 
-    await page.pdf({ path: path.resolve(__dirname, '../public/stephen-oneil-resume.pdf') });
-    await browser.close();
+    if ( !fs.existsSync(`${build_dir}/index.html`) ){
+      console.log("Error generating pdf: build dir doesn't contain an index.html file");
+    } else {
+      const free_port = await getPort();
+
+      const server = await serve_build_dir(free_port);
+
+      await save_pdf(free_port);
+
+      if ( fs.existsSync(`${build_dir}/${pdf_name}`) ){
+        console.log(`Generating pdf successful`);
+      }
+      // not logging the inverse as, pretty sure, anything going wrong before this would have thrown its own error. Confirm that some time?
+
+      server.close(); 
+    }
 })();
