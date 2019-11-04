9eee9ea0a599f9b64033ae439a1512559d58bf33
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-03 19:02:46 -0500

    Move content out of Resume.js

--- src/Resume.js
+++ src/Resume.js
@@ -1,114 +1,43 @@
 import React from 'react';
-import './Resume.scss';
-
 import ReactMarkdown from 'react-markdown';
 
-
-const contact_info = `
-# Stephen O'Neil
-## Full Stack Web App Dev
-(613) 867-7727  
-[github.com/Stephen-ONeil](https://github.com/Stephen-ONeil)  
-[github.com/TBS-EACPD/infobase](https://github.com/TBS-EACPD/infobase)  
-[Stephen.Levi.ONeil@gmail.com](mailto:Stephen.Levi.ONeil@gmail.com)  
-Apt. 7 - 45 Somerset Street, Ottawa, Ontario, K2P 0H3  
-`;
-
-const brief = `
-- Honours B.Sc. Physics-Mathematics, uOttawa
-- **Senior JavaScript developer, full stack** at 
-Treasury Board of Canada Secretariat (TBS)
-  - **4+ years front end**. **React, Lodash, Sass, D3**
-  - **1.5+ years back end, DevOps. Node, GraphQL, MongoDB, Google Cloud**
-- E.g. **GC InfoBase** ([canada.ca/gcinfobase](https://canada.ca/gcinfobase)), a SPA React frontend backed by a serverless GraphQL API, offering data vizualization and exploration vectors
-`;
-
-const developer = `
-## Developer
-- _Prefer done right, right now, over done yesterday. The "done yesterday" mindset is opposed to "still works tomorrow"._  
-&nbsp;&nbsp;&nbsp;&nbsp&nbsp&nbsp;– Me, on my resume
-- JS expert, HTML & CSS proficient. Framework of choice React. Strives to make ethical and user-centric design decisions, from speed to A11y
-- ECMAScript proposal watcher, excited for the pipeline operator and optional chaining + nullish coalescing. Ride the wave of JS progress!
-- Generally borrows heavily from the functional paradigm, favours declarative APIs internally, and loves formulating robust data-driven control-flow  
-- Environment of choice is VS Code and command line tools. Most at home with Linux or Posix (macOS) systems.
-- Git and GitHub fan, with years of experience using and helping others to learn the tool & service
-`;
-
-const operations = `
-## Operations                
-- Strong believer in DevOps, personally bringing both the "Dev" and the "Ops" to the greatest feasible extent
-- Automation and Bash scripts! Getting fancy with CI! Serverless microservices! Easy & safe deploys! It's rewarding and fun.
-`;
-
-const security = `
-## Security
-- Pushing _from_ the left, bringing security-by-design to the design stages of Dev and DevOps tasks
-- Staying up-to-date with SecOps and AppSec news and best practices through blogs, mailing lists, podcasts, and the local OWASP meet-up
-`;
-
-const curriculum_vitae = `
-## Curriculum Vitae
-- **09/2017 - Ongoing** – Senior JavaScript Developer, Expenditure Management Sector (EMS), TBS
-  - Maintaining the GC InfoBase front end source code, at ~50,000 lines of JS. Implementing, and guiding junior Devs' implementation of new features while ensuring continued maintainability
-  - Building backend components for the main SPA, including a GraphQL data-fetching API and an email backend (obviating mailto:)
-  - Hiring, supervising, and mentoring of CO-OP students; 7+ to date
-  - Championing for code standards, code review, testing, CI/CD, and Open Sourcing/working in the open
-  - Transitioned hosting from a static file server administered by a separate team to a self-administered cloud hosting setup, bringing the deployment process from days to minutes, gaining more flexible, faster hosting & a real backend, and doing it all for negligible hosting costs
-  - Providing a security audit function within my team, identifying more, and more relevant, risks and sensitive bugs than our IT security's commissioned App scans
-  
-- **01/2015 - 09/2017** – Developer/Junior Analyst/CO-OP Student, Office of the Chief Human Resources Officer (OCHRO), TBS
-  - Developed and maintained OCHRO's portions of the GC InfoBase
-  - SQL, SAS, Java. Responded to ad-hoc requests for government-wide HR data, automated the periodic ones
-  
-- **09/2012 - 04/2017** – Honours Bachelour of Science, Physics-Mathematics with CO-OP, University of Ottawa
-  - Dean's Honour List 2014-2015 (for an annual GPA of 8.5+)
-  - Founding president of uOttawa Computational Physics Club
-`;
-
-const extracurricular = `
-## Extracurricular
-- *nix nerd, [github.com/ Stephen-ONeil/.dotfiles](https://github.com/Stephen-ONeil/.dotfiles)
-- Local tech meet-ups
-- Reading novels (Sci-fi, fantasy) & comics (web, indie)
-- Listening to music (misc) & podcasts (comedy, tech)
-- Night-time walking
-`;
-
+import './Resume.scss';
+import * as content from './content.js'; 
 
 function Resume(){
   return (
     <div className="resume fcol">
       <div className="frow">
         <section className="fcol fcol__1-half" style={{textAlign: "center"}}>
-          <ReactMarkdown source={contact_info} />
+          <ReactMarkdown source={content.contact_info} />
         </section>
         <section className="fcol fcol__1-half">
           <h2 style={{height: "0em", width: "0em", fontSize: "0em"}}>
             {"Overview"}
           </h2>
-          <ReactMarkdown source={brief} />
+          <ReactMarkdown source={content.brief} />
         </section>
       </div>
       <div className="frow" style={{flexGrow: 1}}>
         <div className="fcol fcol__1-3rd">
           <section className="fcol" style={{height: "100%"}}>
-            <ReactMarkdown source={developer} />
+            <ReactMarkdown source={content.developer} />
           </section>
           <section className="fcol">
-            <ReactMarkdown source={extracurricular} />
+            <ReactMarkdown source={content.extracurricular} />
           </section>
         </div>
         <div className="fcol fcol__2-3rd" style={{flexGrow: 1}}>
           <div className="frow">
             <section className="fcol fcol__1-half">
-              <ReactMarkdown source={operations} />
+              <ReactMarkdown source={content.operations} />
             </section>
             <section className="fcol fcol__1-half">
-              <ReactMarkdown source={security} />
+              <ReactMarkdown source={content.security} />
             </section>
           </div>
           <section className="fcol" style={{height: "100%"}}>
-            <ReactMarkdown source={curriculum_vitae} />
+            <ReactMarkdown source={content.curriculum_vitae} />
           </section>
         </div>
       </div>
---
+++ src/content.js
@@ -0,0 +1,79 @@
+const contact_info = `
+# Stephen O'Neil
+## Full Stack Web App Dev
+(613) 867-7727  
+[github.com/Stephen-ONeil](https://github.com/Stephen-ONeil)  
+[github.com/TBS-EACPD/infobase](https://github.com/TBS-EACPD/infobase)  
+[Stephen.Levi.ONeil@gmail.com](mailto:Stephen.Levi.ONeil@gmail.com)  
+Apt. 7 - 45 Somerset Street, Ottawa, Ontario, K2P 0H3  
+`;
+
+const brief = `
+- Honours B.Sc. Physics-Mathematics, uOttawa
+- **Senior JavaScript developer, full stack** at 
+Treasury Board of Canada Secretariat (TBS)
+  - **4+ years front end**. **React, Lodash, Sass, D3**
+  - **1.5+ years back end, DevOps. Node, GraphQL, MongoDB, Google Cloud**
+- E.g. **GC InfoBase** ([canada.ca/gcinfobase](https://canada.ca/gcinfobase)), a SPA React frontend backed by a serverless GraphQL API, offering data vizualization and exploration vectors
+`;
+
+const developer = `
+## Developer
+- _Prefer done right, right now, over done yesterday. The "done yesterday" mindset is opposed to "still works tomorrow"._  
+&nbsp;&nbsp;&nbsp;&nbsp&nbsp&nbsp;– Me, on my resume
+- JS expert, HTML & CSS proficient. Framework of choice React. Strives to make ethical and user-centric design decisions, from speed to A11y
+- ECMAScript proposal watcher, excited for the pipeline operator and optional chaining + nullish coalescing. Ride the wave of JS progress!
+- Generally borrows heavily from the functional paradigm, favours declarative APIs internally, and loves formulating robust data-driven control-flow  
+- Environment of choice is VS Code and command line tools. Most at home with Linux or Posix (macOS) systems.
+- Git and GitHub fan, with years of experience using and helping others to learn the tool & service
+`;
+
+const operations = `
+## Operations                
+- Strong believer in DevOps, personally bringing both the "Dev" and the "Ops" to the greatest feasible extent
+- Automation and Bash scripts! Getting fancy with CI! Serverless microservices! Easy & safe deploys! It's rewarding and fun.
+`;
+
+const security = `
+## Security
+- Pushing _from_ the left, bringing security-by-design to the design stages of Dev and DevOps tasks
+- Staying up-to-date with SecOps and AppSec news and best practices through blogs, mailing lists, podcasts, and the local OWASP meet-up
+`;
+
+const curriculum_vitae = `
+## Curriculum Vitae
+- **09/2017 - Ongoing** – Senior JavaScript Developer, Expenditure Management Sector (EMS), TBS
+  - Maintaining the GC InfoBase front end source code, at ~50,000 lines of JS. Implementing, and guiding junior Devs' implementation of new features while ensuring continued maintainability
+  - Building backend components for the main SPA, including a GraphQL data-fetching API and an email backend (obviating mailto:)
+  - Hiring, supervising, and mentoring of CO-OP students; 7+ to date
+  - Championing for code standards, code review, testing, CI/CD, and Open Sourcing/working in the open
+  - Transitioned hosting from a static file server administered by a separate team to a self-administered cloud hosting setup, bringing the deployment process from days to minutes, gaining more flexible, faster hosting & a real backend, and doing it all for negligible hosting costs
+  - Providing a security audit function within my team, identifying more, and more relevant, risks and sensitive bugs than our IT security's commissioned App scans
+  
+- **01/2015 - 09/2017** – Developer/Junior Analyst/CO-OP Student, Office of the Chief Human Resources Officer (OCHRO), TBS
+  - Developed and maintained OCHRO's portions of the GC InfoBase
+  - SQL, SAS, Java. Responded to ad-hoc requests for government-wide HR data, automated the periodic ones
+  
+- **09/2012 - 04/2017** – Honours Bachelour of Science, Physics-Mathematics with CO-OP, University of Ottawa
+  - Dean's Honour List 2014-2015 (for an annual GPA of 8.5+)
+  - Founding president of uOttawa Computational Physics Club
+`;
+
+const extracurricular = `
+## Extracurricular
+- *nix nerd, [github.com/ Stephen-ONeil/.dotfiles](https://github.com/Stephen-ONeil/.dotfiles)
+- Local tech meet-ups
+- Reading novels (Sci-fi, fantasy) & comics (web, indie)
+- Listening to music (misc) & podcasts (comedy, tech)
+- Night-time walking
+`;
+
+export {
+  contact_info,
+  brief,
+  developer,
+  operations,
+  security,
+  curriculum_vitae,
+  extracurricular,
+};
