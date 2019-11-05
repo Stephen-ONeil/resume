00969400e951a7e459b1860e7939263d3d585af1
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-05 01:40:09 -0500

    Make dowload icon arrow consistent with other arrows

--- src/DownloadPDF.js
+++ src/DownloadPDF.js
@@ -4,12 +4,11 @@ import './DownloadPDF.scss';
 function DownloadPDF(){
   return (
     <a className="download-pdf-link" href='stephen-oneil-resume.pdf' download>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  x="0px" y="0px" aria-hidden="false">
-          <title>Download resume as PDF</title>
-          <g>
-            <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"></path>
-          </g>
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" transform="scale(0.8) rotate(90)">
+        <title>Download as PDF</title>
+        <polyline transform="scale(0.85) translate(0, 5.5)" points="66.5 36 5.5 72 21.5 36 5.5 0 66.5 36"/>
+        <rect height="62.28571" width="7.14286" y="4.85714" x="63.57143"/>
+      </svg>
     </a>
   );
 }
