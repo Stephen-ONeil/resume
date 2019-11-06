caa3c57d879862c1940a97c9a949aa3896064f70
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-05 22:10:20 -0500

    Caught a missing semicolon

--- src/resume-common.js
+++ src/resume-common.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 export const ResumeSection = ({className, style, section, heading_element, heading_style}) => {
-  const HeadingElement = /^h[1-9]$/.test(heading_element) ? heading_element : 'h1'
+  const HeadingElement = /^h[1-9]$/.test(heading_element) ? heading_element : 'h1';
   return (
     <section className={className} style={style}>
       <HeadingElement style={heading_style}>
