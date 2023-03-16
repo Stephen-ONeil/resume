22b528233c00fe0b3135c1fc272e5206608fcb85
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-03-16 17:09:00 -0400

    Add page-specific config export to pages/index.tsx, move the output: export rule there and add unstable_runtimeJS: false. Now prod builds don't ship ANY JS, in additiona to exporting to pre-rendered HTML

--- next.config.cjs
+++ next.config.cjs
@@ -1,5 +1,4 @@
-const nextConfig = {
-  output: "export",
-};
+// Empty for now, but note that there are page-specific configs set in src/pages/index.tsx!
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,19 @@ import { IconTray } from "src/components/IconTray/IconTray.js";
 import { PaperResume } from "src/resume_layouts/PaperResume.js";
 import { ResponsiveResume } from "src/resume_layouts/ResponsiveResume.js";
 
+export const config = {
+  output: "export", // in prod, export final built site to HTML
+  unstable_runtimeJS: false, // in prod, strip the JS from the exported HTML site; REMINDER: don't do anything that requires runtime JS, haha
+};
+
 export default function Index() {
   return (
     <>
       <Head>
         <title>{"Resume - Stephen O'Neil"}</title>
         <meta
-          content="Resume and personal site of Stephen O'Neil, full-stack developer"
           name="description"
+          content="Resume and personal site of Stephen O'Neil, full-stack developer"
         />
       </Head>
       <main>
