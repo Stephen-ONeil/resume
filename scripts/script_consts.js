25613897e41740aa25c3c673120ac58eb569d6ca
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-12-04 12:06:18 -0500

    Update build_dir constant used by scripts

--- scripts/script_consts.js
+++ scripts/script_consts.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-export const build_dir = path.resolve(__dirname, "../out");
+export const build_dir = path.resolve(__dirname, "../dist");
 
 export const pdf_name = "stephen-oneil-resume.pdf";
 
