06bb2539c1b24ff8ee23997ef46f2e37dee240d1
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-12-09 19:29:34 -0500

    Wasn't originally intending to totally dereactify, but ended up being a natural result of astroifying, haha. Drop react dependencies and tooling

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
-import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://resume.oneil.online/",
   base: "/",
   trailingSlash: "always",
-  integrations: [react()],
   output: "static",
   build: {
     inlineStylesheets: "always",
--- eslint.config.ts
+++ eslint.config.ts
@@ -3,22 +3,18 @@ import tsParser from "@typescript-eslint/parser";
 import { defineConfig, globalIgnores } from "eslint/config";
 import { configs as astroConfigs } from "eslint-plugin-astro";
 import importPlugin from "eslint-plugin-import";
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import prettierPluginRecommended from "eslint-plugin-prettier/recommended";
-import reactPlugin from "eslint-plugin-react";
 import globals from "globals";
 import { configs as tsConfigs } from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["node_modules", "dist", ".astro"]),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["**/*.{js,ts}"],
 
     extends: [
       js.configs.recommended,
       tsConfigs.recommended,
-      reactPlugin.configs.flat.recommended,
-      jsxA11yPlugin.flatConfigs.recommended,
       importPlugin.flatConfigs.recommended,
       importPlugin.flatConfigs.typescript,
     ],
@@ -42,10 +38,6 @@ export default defineConfig([
           moduleDirectory: ["./src/", "node_modules/"],
         },
       },
-
-      react: {
-        version: "detect",
-      },
     },
 
     rules: {
@@ -53,11 +45,6 @@ export default defineConfig([
       "no-undef": "error",
       "no-debugger": "warn",
       "prefer-const": "warn",
-
-      "react/no-find-dom-node": "warn",
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-key": ["warn"],
 
       "@typescript-eslint/explicit-module-boundary-types": ["off"],
       "@typescript-eslint/no-empty-interface": ["off"],
@@ -108,6 +95,6 @@ export default defineConfig([
     },
   },
   astroConfigs["flat/recommended"],
-  //astroConfigs["jsx-a11y-recommended"], // see https://github.com/ota-meshi/eslint-plugin-astro/pull/496
+  astroConfigs["jsx-a11y-recommended"],
   prettierPluginRecommended,
 ]);
