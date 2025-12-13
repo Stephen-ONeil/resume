9899802e34d95c8d40568dae3a1209752dd0ff62
Stephen-ONeil <stephenlevioneil@gmail.com>
2025-12-13 16:39:23 -0500

    Replace my old generate_pdf.ts script with astro-pdf

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import markdoc from "@astrojs/markdoc";
 import { defineConfig } from "astro/config";
 
+import pdf from "astro-pdf";
+
 export default defineConfig({
   site: "https://resume.oneil.online/",
   base: "/",
@@ -15,7 +17,10 @@ export default defineConfig({
       cssMinify: true,
     },
   },
-  integrations: [markdoc({ allowHTML: true })],
+  integrations: [
+    markdoc({ allowHTML: true }),
+    pdf({ pages: { "/": "stephen-oneil-resume.pdf" } }),
+  ],
   experimental: {
     contentIntellisense: true,
   },
